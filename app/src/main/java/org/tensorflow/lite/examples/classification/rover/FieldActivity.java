1f93aabc227ceb1ac8e372d6832ae37a91f3f2b8
Christian <iosif.christian@gmail.com>
2019-11-20 20:01:11 -0500

    -FieldActivity will be for deciding field params -Roverparams include basic rover setters -Swarm is for communicating with the arduino

---
+++ app/src/main/java/org/tensorflow/lite/examples/classification/rover/FieldActivity.java
@@ -0,0 +1,113 @@
+package org.tensorflow.lite.examples.classification.rover;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class FieldActivity {
+    double fieldWidth;
+    double fieldLength;
+
+    int blockWidth;
+    int blockLength;
+    int blocksPerLane;
+    int lanes;
+
+    static int notSearched = 0;
+    static int searched = 1;
+    static int found = 2;
+
+    //Sets bockWidth, blockLength, blocksPerLane, lanes
+    void calculateFieldParams(double fieldWidth, double fieldLength) {
+
+    }
+
+    //Does math to calculate the degree at which the rover should turn its wheels in order to get
+    //back on track
+    void calculateTurningRadius() {
+
+    }
+
+    //Creates the grid similar to battleship (Map<string> grid = new Map())
+    //Use above values to create A(1) - A(blocksPerLane), B(1) - B(blocksPerLane), etc.
+    void createGrid() {
+
+    }
+
+    //Changes value next to key from notSearched to searched or found
+    void updateGrid() {
+
+    }
+
+    Map<Integer, Integer> grid = new Map<Integer, Integer>() {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean containsKey(@Nullable Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean containsValue(@Nullable Object value) {
+            return false;
+        }
+
+        @Nullable
+        @Override
+        public Integer get(@Nullable Object key) {
+            return null;
+        }
+
+        @Nullable
+        @Override
+        public Integer put(Integer key, Integer value) {
+            return null;
+        }
+
+        @Nullable
+        @Override
+        public Integer remove(@Nullable Object key) {
+            return null;
+        }
+
+        @Override
+        public void putAll(@NonNull Map<? extends Integer, ? extends Integer> m) {
+
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @NonNull
+        @Override
+        public Set<Integer> keySet() {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Collection<Integer> values() {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Set<Entry<Integer, Integer>> entrySet() {
+            return null;
+        }
+
+    };
+}
---
+++ app/src/main/java/org/tensorflow/lite/examples/classification/rover/Swarm.java
@@ -0,0 +1,55 @@
+package org.tensorflow.lite.examples.classification.rover;
+
+public class Swarm {
+    //static (insert datatype here) startingDirection;
+    //(insert datatype here) currentDirection
+
+    //Initializes the rover
+    void initializeRover(){
+        //usesFieldParams
+        //usesCurrentDirection()
+        //alignWheels()
+    }
+
+    //Turns wheels all the way to the left, then right, then aligns to the
+    //middle
+    void alignWheels(){
+
+    }
+
+    //Turns wheels in proper direction and angle to realign arduino
+    //back into original orientation while continuing forward
+    void adjustWheelAlignment(){
+
+    }
+
+    //Called when rover reaches the beginning of the last block
+    //Uses fieldParams.getTurningRadius() to decide the angle
+    //to turn the rover into the other lane
+    void turnRight(){
+
+    }
+
+
+    //Called when rover reaches the beginning of the last block
+    //Uses fieldParams.getTurningRadius() to decide the angle
+    //to turn the rover into the other lane
+    void turnLeft(){
+
+    }
+
+    void goForward(){
+
+    }
+
+    //Called when TensorFlow object confidence %>75 && soccer ball
+    //Sends notification to ServiceNow. Notifies other raptors
+    void found(){
+
+    }
+
+    void stop(){
+
+    }
+
+}
