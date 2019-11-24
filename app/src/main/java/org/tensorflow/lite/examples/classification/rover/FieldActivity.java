72d377d323ca26c2dead9e20d19d97ce646c5e4b
Christian <iosif.christian@gmail.com>
2019-11-24 10:46:54 -0500

    -Finished FieldActivity with the grid created, and both update methods

--- app/src/main/java/org/tensorflow/lite/examples/classification/rover/FieldActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/rover/FieldActivity.java
@@ -1,12 +1,5 @@
 package org.tensorflow.lite.examples.classification.rover;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
-
 public class FieldActivity {
     double fieldWidth;
     double fieldLength;
@@ -21,95 +14,32 @@ public class FieldActivity {
     static int searched = 1;
     static int found = 2;
 
+    int[][] grid = new int[lanes][blocksPerLane];
+
+
     //Sets blocksPerLane, lanes
     void calculateFieldParams(double fieldWidth, double fieldLength) {
         lanes = (int) (fieldWidth / blockWidth);
         blocksPerLane = (int) (fieldLength / blockLength);
     }
 
-    //Does math to calculate the degree at which the rover should turn its wheels in order to get
-    //back on track
-    void calculateTurningRadius() {
-
-    }
-
-    //Creates the grid similar to battleship (Map<string> grid = new Map())
-    //Use above values to create A(1) - A(blocksPerLane), B(1) - B(blocksPerLane), etc.
+    //Creates the grid similar to battleship
     void createGrid() {
-
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                grid[i][j] = notSearched;
+            }
+        }
     }
 
-    //Changes value next to key from notSearched to searched or found
-    void updateGrid() {
-
+    //Changes value next to key from notSearched to searched
+    void searchedBlock(int lane, int block) {
+        grid[lane][block] = searched;
     }
 
-    Map<Integer, Integer> grid = new Map<Integer, Integer>() {
-        @Override
-        public int size() {
-            return 0;
-        }
+    //Changes value next to key from notSearched to found
+    void foundInBlock(int lane, int block) {
+        grid[lane][block] = found;
+    }
 
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
-
-        @Override
-        public boolean containsKey(@Nullable Object o) {
-            return false;
-        }
-
-        @Override
-        public boolean containsValue(@Nullable Object value) {
-            return false;
-        }
-
-        @Nullable
-        @Override
-        public Integer get(@Nullable Object key) {
-            return null;
-        }
-
-        @Nullable
-        @Override
-        public Integer put(Integer key, Integer value) {
-            return null;
-        }
-
-        @Nullable
-        @Override
-        public Integer remove(@Nullable Object key) {
-            return null;
-        }
-
-        @Override
-        public void putAll(@NonNull Map<? extends Integer, ? extends Integer> m) {
-
-        }
-
-        @Override
-        public void clear() {
-
-        }
-
-        @NonNull
-        @Override
-        public Set<Integer> keySet() {
-            return null;
-        }
-
-        @NonNull
-        @Override
-        public Collection<Integer> values() {
-            return null;
-        }
-
-        @NonNull
-        @Override
-        public Set<Entry<Integer, Integer>> entrySet() {
-            return null;
-        }
-
-    };
 }
